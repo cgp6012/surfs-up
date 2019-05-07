acccfb445ca2a9cc88fd2b54637834cd1c229124
cgp6012 <cgp6012@gmail.com>
2019-05-07 11:02:24 -0400

    packages added, star rating component started

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,5 +1,11 @@
 // client/src/App.js
 import React, { Component } from "react";
+// Add the following
+import { fontawesome } from '@fortawesome/fontawesome'
+import { solid } from '@fortawesome/fontawesome-free-solid'
+import { regular } from '@fortawesome/fontawesome-free-regular'
+
+fontawesome.library.add(solid, regular)
 
 class App extends Component {
   render() {
--- server/client/src/components/StarRating.js
+++ server/client/src/components/StarRating.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default class StarRating extends Component {
   static defaultProps = {
+    //controls the lowerbound value of our rating. Defaults to 0
     minRating: 0,
+    //controls the upperbound value of our rating. Defaults to 10
     maxRating: 10,
+    //actual value of our rating. Defaults to 5
     rating: 5,
+    //controls how many rating points it takes to render a full star. Defaults to 2, 
+    //which means it takes a rating of 2 to render a single star. With a default rating of 5, this will result in rendering two and a half stars.
     starRatio: 2,
+    //limit for how large the min/max rating values can be
     limit: 1000
   }
 
